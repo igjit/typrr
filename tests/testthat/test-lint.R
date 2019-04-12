05c7d2e2cc03a05ad344b140bf08e77233dc8ba3
igjit <igjit1@gmail.com>
2019-04-12 17:26:45 +0900

    Refactor: extract to function

--- tests/testthat/test-lint.R
+++ tests/testthat/test-lint.R
@@ -1,9 +1,13 @@
 context("lint.R")
 
-test_that("source_expressions", {
-  lines <- c("a <- 1", "a + 2")
+to_file <- function (lines) {
   file <- tempfile()
   writeLines(lines, file)
-  expressions <- source_expressions(file)
+  file
+}
+
+test_that("source_expressions", {
+  lines <- c("a <- 1", "a + 2")
+  expressions <- source_expressions(to_file(lines))
   expect_equal(purrr::map_chr(expressions, "lines"), lines)
 })
