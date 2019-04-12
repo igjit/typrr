eecea1ad8a20fe838467690882b2203820d89818
igjit <igjit1@gmail.com>
2019-04-12 17:27:11 +0900

    Test type_check

--- tests/testthat/test-lint.R
+++ tests/testthat/test-lint.R
@@ -11,3 +11,9 @@ test_that("source_expressions", {
   expressions <- source_expressions(to_file(lines))
   expect_equal(purrr::map_chr(expressions, "lines"), lines)
 })
+
+test_that("type_check", {
+  expect_null(type_check(to_file(c("a <- 1", "a + 2"))))
+  expect_equal(type_check(to_file(c("a <- 1", "a + 'foo'"))),
+               list(line = 2, column = 1, message = "type error"))
+})
