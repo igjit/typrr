305d0f553d1f563c112e65d9d3e07e252b2af0ff
igjit <igjit1@gmail.com>
2019-05-02 06:41:54 +0900

    Add test patterns

--- tests/testthat/test-lint.R
+++ tests/testthat/test-lint.R
@@ -10,4 +10,12 @@ test_that("type_check", {
   expect_null(type_check(to_file(c("a <- 1", "a + 2"))))
   expect_equal(type_check(to_file(c("a <- 1", "a + 'foo'"))),
                list(line = 2, column = 1, message = "type error"))
+
+  expect_null(type_check(to_file(c("add2 <- function(x) x + 2", "add2(40)"))))
+  expect_equal(type_check(to_file(c("add2 <- function(x) x + 2", "add2('abc')"))),
+               list(line = 2, column = 1, message = "type error"))
+
+  expect_null(type_check(to_file(c("foo <- function(x) 40", "foo(0) + 2"))))
+  expect_equal(type_check(to_file(c("foo <- function(x) TRUE", "foo(0) + 2"))),
+               list(line = 2, column = 1, message = "type error"))
 })
